7205a09d0c85d15b9709f9f54eefb72523ed4f2a
Olivia Philomena Moser <oliviaphilomena.moser@gmail.com>
2023-06-22 08:34:40 +0200

    Changing double quotes to single quotes where possible

--- pygmy/data/qrcode-updater.py
+++ pygmy/data/qrcode-updater.py
@@ -4,9 +4,9 @@ from io import BytesIO
 import sqlite3
 
 def lookForNull():
-    print("Looking for null values")
+    print('Looking for null values')
     try:
-        conn = sqlite3.connect("pygmy-update.db")
+        conn = sqlite3.connect('pygmy.db')
         cur = conn.cursor()
         cur.execute('SELECT id, qr_code, short_code FROM link WHERE qr_code IS NULL')
         table = cur.fetchall()
@@ -18,27 +18,27 @@ def lookForNull():
             print(qr_code)
             cur.execute("UPDATE link SET qr_code = '%s' WHERE link.id = %s" % (qr_code, row[0]))
         conn.commit()
-        print("Affected rows " + str(cur.rowcount))
+        print('Affected rows ' + str(cur.rowcount))
         conn.close()
     except:
-        print("DB connection failed")
+        print('DB connection failed')
 
 def generateQrCode(shorted):
-    print("Generating the Qr-Code")
+    print('Generating the Qr-Code')
     qr = qrcode.QRCode(
         version=None,
         error_correction=qrcode.constants.ERROR_CORRECT_M,
         box_size=20,
         border=4,
     )
-    qr.add_data("https://herme.li/" + shorted)
+    qr.add_data('https://herme.li/' + shorted)
     qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color='black', back_color='white')
     img_byte_arr = BytesIO()
     img.save(img_byte_arr, format='PNG')
     img_byte_arr = img_byte_arr.getvalue()
     qr_code = b64encode(img_byte_arr)
     return qr_code
 
-print("Updating Qr-Codes")
+print('Updating Qr-Codes')
 lookForNull()
